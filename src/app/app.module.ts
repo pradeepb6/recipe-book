29415e3470429f1a532b36fd4447adc8379d55b7
pradeepb <pradeepbaradur6@gmail.com>
2016-10-02 22:16:54 +0200

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
-import { DropDownDirective } from './drop-down.directive';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header.component';
+import {RecipesComponent} from './recipes/recipes.component';
+import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
+import {RecipeItemComponent} from './recipes/recipe-list/recipe-item.component';
+import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingListAddComponent} from './shopping-list/shopping-list-add.component';
+import {DropDownDirective} from './drop-down.directive';
 import {RecipeService} from "./recipes/recipe.service";
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
+import {routing} from "./app.routing";
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    ShoppingListComponent,
-    ShoppingListAddComponent,
-    DropDownDirective
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [RecipeService, ShoppingListService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeItemComponent,
+        RecipeDetailComponent,
+        ShoppingListComponent,
+        ShoppingListAddComponent,
+        DropDownDirective,
+        RecipeEditComponent,
+        RecipeStartComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        routing
+    ],
+    providers: [RecipeService, ShoppingListService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,23 +1,46 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {Recipe} from "../recipe";
-import {Input} from "@angular/core/src/metadata/directives";
 import {ShoppingListService} from "../../shopping-list/shopping-list.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Subscription} from "rxjs";
+import {RecipeService} from "../recipe.service";
 
 @Component({
     selector: 'rb-recipe-detail',
     templateUrl: './recipe-detail.component.html',
     styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent implements OnInit {
-    @Input() selectedRecipe: Recipe;
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+    private subscription: Subscription;
+    selectedRecipe: Recipe;
+    private recipeIndex: number;
 
-    constructor(private shoppingListService: ShoppingListService) {
+    constructor(private shoppingListService: ShoppingListService, private activatedRoute: ActivatedRoute,
+    private recipeService: RecipeService, private router: Router) {
     }
 
     ngOnInit() {
+        this.subscription = this.activatedRoute.params.subscribe(
+            (params: any) => {
+                this.recipeIndex = params['id'];
+                this.selectedRecipe = this.recipeService.getRecipe(this.recipeIndex);
+            }
+        )
     }
 
+    onEdit() {
+        this.router.navigate(['/recipes', this.recipeIndex, 'edit']);
+    }
+
+    onDelete() {
+        this.recipeService.deleteRecipe(this.selectedRecipe);
+        this.router.navigate(['/recipes']);
+    }
     onAddToShoppingList() {
         this.shoppingListService.addItems(this.selectedRecipe.ingredients);
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
     }
 }
